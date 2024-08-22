e374b19b3496197928d841d002de6c651b65ba5f
Luciano Griffa <lucianogriffa@protonmail.com>
2024-08-22 11:56:05 -0300

    Create install_requirements.sh

---
+++ .github/scripts/install_requirements.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! command -v pip &> /dev/null; then
+  echo "Pip install..."
+  python3 -m ensurepip --upgrade
+fi
+
+echo "Install Dependencies..."
+pip install -r requirements.txt
+
+echo "🏃 Run Script..."
+python3 ../../index.py
