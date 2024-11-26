c20d9b0c5f0e9a48e9993769e1ff874d4ae0341e
Luciano Griffa <lucianogriffa@protonmail.com>
2024-11-26 11:30:53 -0300

    Update start.sh

--- .github/scripts/start.sh
+++ .github/scripts/start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo "/home/"
-ls -l /home/
-echo "which go"
-which go
-echo "/home/runner/go"
-ls -l /home/runner/go
+go version
+export PATH=$PATH:/usr/local/go/bin
+source ~/.profile
+mkdir -p ~/go/{bin,src,pkg}
+ls -l ~/go
+export GOROOT=/usr/local/go
+export GOPATH="$HOME/go"
+export GOBIN="$GOPATH/bin"
+source ~/.profile
+go version
+go env
