a25d70bd4d4e7e93f6ac80eb8de8b5614f6c03ff
Luciano Griffa <lucianogriffa@protonmail.com>
2024-11-26 11:34:08 -0300

    Update start.sh

--- .github/scripts/start.sh
+++ .github/scripts/start.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
  
-sudo apt install golang -y
-go version
-export PATH=$PATH:/usr/local/go/bin
-source ~/.profile
-mkdir -p ~/go/{bin,src,pkg}
-ls -l ~/go
-export GOROOT=/usr/local/go
-export GOPATH="$HOME/go"
-export GOBIN="$GOPATH/bin"
-source ~/.profile
+which go
 go version
 go env
