040c3971c3c68d69a1004917370a1fbefd5391d6
Luciano Griffa <lucianogriffa@protonmail.com>
2024-11-26 12:07:41 -0300

    Update index.py

--- index.py
+++ index.py
@@ -29,6 +29,9 @@ for tag in tags:
       if message not in sent_messages:
         # With Notify
         os.system(f"echo '{message}' > temp_notify.txt" )
+        os.system(f"ls -l $HOME/")
+        os.system(f"ls -l $HOME/go/bin/")
+        os.system(f"ls -l")
         os.system(f"cat temp_notify.txt | $HOME/go/bin/notify -pc config.yaml -p discord,telegram -bulk")
         print("🟢¡Success!!")
         with open("telegram_messages.txt", "a", encoding="utf-8") as file:
